d7ae1da69ae367c219e99b9a590881f049d53ec1
Niji Saeba Media Noche <mathereall@gmail.com>
2024-06-14 21:26:21 -0600

    initial

---
+++ Maptail/settings/dev.py
@@ -0,0 +1,22 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "7z3_t5!4pej52!oklg8#)%zc5es5k(_c2#kyl(kno2g6lm+wm^"
+
+ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS += [  # noqa
+    "django_sass",
+]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local import *  # noqa
+except ImportError:
+    pass
