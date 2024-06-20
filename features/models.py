f3ecd402b5a6e181333853e5ca084f67baeb4f8e
Niji Saeba Media Noche <mathereall@gmail.com>
2024-06-20 11:49:12 -0400

    pygeoapi

--- features/models.py
+++ features/models.py
@@ -37,7 +37,7 @@ class LineString(LockableWorkFlowDraftStateRevisionModelBaseMixin):
     data = models.JSONField(null=True, blank=True)
     geom = models.LineStringField(srid=settings.DATA_FEATURES_SRID)
     layer = ParentalKey(LineStringVectorLayer, on_delete=models.CASCADE, related_name="lines")
-    file_id = models.CharField(max_length=40, null=True, blank=True)
+    file_uuid = models.CharField(max_length=40, null=True, blank=True)
 
     def __str__(self):
         return f"{self.layer.name}: {self.id}"
@@ -47,7 +47,7 @@ class Polygon(LockableWorkFlowDraftStateRevisionModelBaseMixin):
     data = models.JSONField(null=True, blank=True)
     geom = models.PolygonField(srid=settings.DATA_FEATURES_SRID)
     layer = ParentalKey(PolygonVectorLayer, on_delete=models.CASCADE, related_name="polygons")
-    file_id = models.CharField(max_length=40, null=True, blank=True)
+    file_uuid = models.CharField(max_length=40, null=True, blank=True)
 
     def __str__(self):
         return f"{self.layer.name}: {self.id}"
@@ -57,7 +57,7 @@ class MultiPoint(LockableWorkFlowDraftStateRevisionModelBaseMixin):
     data = models.JSONField(null=True, blank=True)
     geom = models.MultiPointField(srid=settings.DATA_FEATURES_SRID)
     layer = ParentalKey(MultiPointVectorLayer, on_delete=models.CASCADE, related_name="multipoints")
-    file_id = models.CharField(max_length=40, null=True, blank=True)
+    file_uuid = models.CharField(max_length=40, null=True, blank=True)
 
     def __str__(self):
         return f"{self.layer.name}: {self.id}"
@@ -67,7 +67,7 @@ class MultiLineString(LockableWorkFlowDraftStateRevisionModelBaseMixin):
     data = models.JSONField(null=True, blank=True)
     geom = models.MultiLineStringField(srid=settings.DATA_FEATURES_SRID)
     layer = ParentalKey(MultiLineStringVectorLayer, on_delete=models.CASCADE, related_name="multilines")
-    file_id = models.CharField(max_length=40, null=True, blank=True)
+    file_uuid = models.CharField(max_length=40, null=True, blank=True)
 
     def __str__(self):
         return f"{self.layer.name}: {self.id}"
@@ -77,7 +77,7 @@ class MultiPolygon(LockableWorkFlowDraftStateRevisionModelBaseMixin):
     data = models.JSONField(null=True, blank=True)
     geom = models.MultiPolygonField(srid=settings.DATA_FEATURES_SRID)
     layer = ParentalKey(MultiPolygonVectorLayer, on_delete=models.CASCADE, related_name="multipolygons")
-    file_id = models.CharField(max_length=40, null=True, blank=True)
+    file_uuid = models.CharField(max_length=40, null=True, blank=True)
 
     def __str__(self):
         return f"{self.layer.name}: {self.id}"
@@ -87,7 +87,7 @@ class GeometryCollection(LockableWorkFlowDraftStateRevisionModelBaseMixin):
     data = models.JSONField(null=True, blank=True)
     geom = models.GeometryCollectionField(srid=settings.DATA_FEATURES_SRID)
     layer = ParentalKey(GeometryCollectionVectorLayer, on_delete=models.CASCADE, related_name="geometrycollections")
-    file_id = models.CharField(max_length=40, null=True, blank=True)
+    file_uuid = models.CharField(max_length=40, null=True, blank=True)
 
     def __str__(self):
         return f"{self.layer.name}: {self.id}"
