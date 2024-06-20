4568b6b5bfc1b9c3b14c81b6eb83d1d8999c0631
Niji Saeba Media Noche <mathereall@gmail.com>
2024-06-20 06:52:48 -0400

    pygeoapi

--- resources/urls.py
+++ resources/urls.py
@@ -1,4 +1,6 @@
-from .viewsets import (ResourceViewSet, PointVectorLayerViewSet, RasterLayerViewSet, DataTableViewSet, RemoteWMSViewSet,
+from .viewsets import (ResourceViewSet, RasterLayerViewSet, DataTableViewSet, RemoteWMSViewSet,
+                        PointVectorLayerViewSet, LineStringVectorLayerViewSet, PolygonVectorLayerViewSet,
+                        MultiPointVectorLayerViewSet, MultiLineStringVectorLayerViewSet, MultiPolygonVectorLayerViewSet,
                        RemoteWFSViewSet)
 from base.router import drf_default_router
 
@@ -6,11 +8,11 @@ urlpatterns = []
 
 drf_default_router.register('rest/v1/resources/resources', ResourceViewSet, basename='resources-resources')
 drf_default_router.register('rest/v1/resources/point-vector-layers', PointVectorLayerViewSet, basename='resources-point-vector-layers')
-drf_default_router.register('rest/v1/resources/linestring-vector-layers', PointVectorLayerViewSet, basename='resources-linestring-vector-layers')
-drf_default_router.register('rest/v1/resources/polygon-vector-layers', PointVectorLayerViewSet, basename='resources-polygon-vector-layers')
-drf_default_router.register('rest/v1/resources/multipoint-vector-layers', PointVectorLayerViewSet, basename='resources-multipoint-vector-layers')
-drf_default_router.register('rest/v1/resources/multilinestring-vector-layers', PointVectorLayerViewSet, basename='resources-multilinestring-vector-layers')
-drf_default_router.register('rest/v1/resources/multipolygon-vector-layers', PointVectorLayerViewSet, basename='resources-multipolygon-vector-layers')
+drf_default_router.register('rest/v1/resources/linestring-vector-layers', LineStringVectorLayerViewSet, basename='resources-linestring-vector-layers')
+drf_default_router.register('rest/v1/resources/polygon-vector-layers', PolygonVectorLayerViewSet, basename='resources-polygon-vector-layers')
+drf_default_router.register('rest/v1/resources/multipoint-vector-layers', MultiPointVectorLayerViewSet, basename='resources-multipoint-vector-layers')
+drf_default_router.register('rest/v1/resources/multilinestring-vector-layers', MultiLineStringVectorLayerViewSet, basename='resources-multilinestring-vector-layers')
+drf_default_router.register('rest/v1/resources/multipolygon-vector-layers', MultiPolygonVectorLayerViewSet, basename='resources-multipolygon-vector-layers')
 drf_default_router.register('rest/v1/resources/rasterlayers', RasterLayerViewSet, basename='resources-rasterlayers')
 drf_default_router.register('rest/v1/resources/datatables', DataTableViewSet, basename='resources-datatables')
 drf_default_router.register('rest/v1/resources/remotewms', RemoteWMSViewSet, basename='resources-remotewms')
